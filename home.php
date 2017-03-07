b15e2ef901c43f966519722da0da92e179a8dcee
Katherine Dix <kathdix@seas.upenn.edu>
2017-03-06 21:37:46 -0500

    added some bootstrap

--- home.php
+++ home.php
@@ -1,8 +1,28 @@
+<!DOCTYPE html>
 <html>
 	<head>
 		<title>Dear Diary</title>
+		<meta charset="utf-8">
+  		<meta name="viewport" content="width=device-width, initial-scale=1">
+  		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		
 	</head>
+	<?php
+		session_start();
+		if($_SESSION['user']) {
+
+		} else {
+			header("location:index.php");
+		}
+		$user = $_SESSION['user'];
+	?>
 	<body>
-		<h2>Home</h2>
+		<?php include "navbar.php" ?>
+		<div class="container-fluid">
+  			<h1>My First Bootstrap Page</h1>
+  			<p>This is some text.</p> 
+		</div>
 	</body>
 </html>
---
+++ navbar.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-default">
+  	<div class="container-fluid">
+    	<div class="navbar-header">
+      		<a class="navbar-brand" href="#">Dear Diary...</a>
+    	</div>
+    	<ul class="nav navbar-nav">
+      		<li class="active"><a href="#">Home</a></li>
+      		<li><a href="#">My Profile</a></li>
+      		<li><a href="#">Make New Entry</a></li>
+      		<li><a href="#">View My Diary</a></li>
+    	</ul>
+    	<form class="navbar-form navbar-left">
+      		<div class="input-group">
+        		<input type="text" class="form-control" placeholder="Search">
+        		<div class="input-group-btn">
+          			<button class="btn btn-default" type="submit">
+            		<i class="glyphicon glyphicon-search"></i>
+          			</button>
+        		</div>
+      		</div>
+    	</form>
+    	<ul class="nav navbar-nav navbar-right">
+      		<li><a href="#"><span class="glyphicon glyphicon-user"></span> Settings</a></li>
+      		<li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+    	</ul>
+  	</div>
+</nav>
