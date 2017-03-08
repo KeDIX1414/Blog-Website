467a30806e36e2f9d007ca48db118813d7c1d09a
Katherine Dix <kathdix@seas.upenn.edu>
2017-03-07 22:49:27 -0500

    added css

--- pages/register.php
+++ pages/register.php
@@ -5,19 +5,52 @@
 		<meta charset="utf-8">
     	<meta name="viewport" content="width=device-width, initial-scale=1">
   		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  		<link rel="stylesheet" href="public/css/base.css">
   		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	</head>
 	<body>
 		<?php include "navbar_notloggedin.php" ?>
-		<h2> Registration Page</h2>
-		<a href="../index.php">Click here to go back</a></br></br>
-		<form action="register.php" method="POST">
-			Enter Email Address: <input type="text" name="email" required="required" /></br>
-			Enter Username: <input type="text" name="username" required="required" /></br>
-			Enter Password: <input type="text" name="password" required="required" /> </br>
-			<input type="submit" value="Register" />
-		</form>
+		<h1>Register Today</h1>
+			<div class="row">
+				<div class="col-sm-2"></div>
+				<div class="col-sm-8">
+					<form action="index.php?page=register" method="POST">
+						<div class="form-group">
+                			<label for="firstName">First Name *</label>
+                			<input type="text" name="firstName" class="form-control">
+            			</div>
+            			<div class="form-group">
+                			<label for="lastName">Last Name *</label>
+                			<input type="text" name="lastName" class="form-control">
+            			</div>
+            			<div class="form-group">
+                			<label for="lastName">Pen Name *</label>
+                			<input type="text" name="penName" class="form-control">
+            			</div>
+            			<div class="form-group">
+                			<label for="email">Email *</label>
+                			<input type="email" name="email" class="form-control">
+            			</div>
+            			<div class="form-group">
+                			<label for="password">Password *</label>
+                			<input type="password" name="password" class="form-control">
+            			</div>
+            			<div class="form-group">
+                			<label for="passwordConfirmation">Confirm Password *</label>
+                			<input type="password" name="passwordConfirmation" class="form-control">
+                			<p class="help-block">Re-enter the above password.</p>
+            			</div>
+            			<div class="form-group">
+                			<label for="birthday">Birthday *</label>
+                			<input type="date" name="birthday" class="form-control">
+            			</div>
+            			<button type="submit" class="btn btn-success">Sign Up</button>
+            			
+					</form>
+				</div>
+				<div class="col-sm-2"></div>
+			</div>				
 	</body>
 </html>
 
